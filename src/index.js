6673a5ec010de96f5c9d648ec073f7cf2809aff4
abduamal <abduamal@ymail.com>
2021-06-05 08:06:10 -0500

    rumblings...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("in index.js")
+const endPoint = "http://localhost:3000/shops"
+
+document.addEventListener('DOMContentLoaded', () => {
+  getShops()
+}) 
+
+function getShops(){
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(shops => {
+    console.log(shops)
+  })
+}
