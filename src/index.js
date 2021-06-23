9c75fc7cc2c244337f02b0e38e77500337456c40
Amal Abdu <abduamal@ymail.com>
2021-06-23 18:37:49 -0500

    Shop POST Request

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const endPoint = "http://localhost:3000/shops"
 // When the DOMContentLoads I want to make a...
 document.addEventListener('DOMContentLoaded', () => {
   getShops()
+
+  const createShopForm = document.querySelector("#create-shop-form")
+
+  createShopForm.addEventListener("submit", (e)=> createShopHandler(e) )
 })
+
 // ...get request translates our backend data into json
 function getShops(){
   fetch(endPoint)
@@ -16,12 +21,46 @@ function getShops(){
       const shopMarkup = `
       <div data-id=${shop.id}>
         <h3>${shop.attributes.name}</h3>
-        <h4>${shop.attributes.industry}</h4>
+        <h5>${shop.attributes.industry}</h5>
         <button data-id=${shop.id}>edit</button>
       </div>
       <br><br>`;
 // I can then add that function to the shop container which will then be rendered in the browser
       document.querySelector('#shop-container').innerHTML += shopMarkup
     })
+  })
+}
+
+function createShopHandler(e){
+  e.preventDefault()
+  const nameInput = document.querySelector('#input-name').value
+  const industryInput = document.querySelector('#input-industry').value
+  shopFetch(nameInput, industryInput)
+}
+function shopFetch(name, industry){
+  // confirm that values are coming in correctly
+  console.log(name, industry)
+  // body building
+  const bodyData = {name, industry}
+  fetch(endPoint, {
+    // using the post method because I am making a post request
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(shop => {
+    console.log(shop)
+    const shopData = shop.data.attributes
+    // render JSON response
+    // refactor the following into a render function to DRY this up!
+    const shopMarkup = `
+    <div data-id=${shop.id}>
+      <h3>${shopData.name}</h3>
+      <h5>${shopData.industry}</h5>
+      <button data-id=${shop.id}>edit</button>
+    </div>
+    <br><br>`;
+    document.querySelector('#shop-container').innerHTML += shopMarkup
   })
 }
