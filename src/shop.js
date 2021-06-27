4adc62b27df0d0d030d53effa9b13dcdd21b62b8
Amal Abdu <abduamal@ymail.com>
2021-06-26 20:48:04 -0500

    Add edit form for the Shop class

--- src/shop.js
+++ src/shop.js
@@ -21,5 +21,22 @@ class Shop {
     return this.all.find(shop => shop.id === id);
   }
 
+  renderShopUpdateForm(){
+    return `
+    <form data-id="${this.id}">
+      <h3>Edit a Shop!</h3>
+
+      <label>Name</label>
+      <input id="input-name" type="text" name="name" value="${this.name}" class="input-text">
+      <br><br>
+
+      <label>Industry</label>
+      <input id="input-industry" type="text" name="industry" value="${this.industry}" class="input-text">
+      <br><br
+
+      <input id='edit-button' type="submit" name="submit" value="Edit Shop" class="submit">
+    </form>
+    `;
+  }
 }
 Shop.all = []
